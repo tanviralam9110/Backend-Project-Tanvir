21321cab7a6e55f97b0c4de6d2214da8a6406d71
Tanvir <tanviralam3524@gmail.com>
2026-04-21 00:55:10 +0530

    First commit 01

---
+++ Backend-Assignment-Tanvir/src/main/java/com/Backend/Assignment/Tanvir/BackendAssignmentTanvirApplication.java
@@ -0,0 +1,13 @@
+package com.Backend.Assignment.Tanvir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAssignmentTanvirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAssignmentTanvirApplication.class, args);
+	}
+
+}
---
+++ Backend-Assignment-Tanvir/src/test/java/com/Backend/Assignment/Tanvir/BackendAssignmentTanvirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Backend.Assignment.Tanvir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAssignmentTanvirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
